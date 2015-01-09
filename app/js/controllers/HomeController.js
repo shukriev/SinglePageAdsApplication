b734249c400fac11c15c43c293d4ec02e281e278
Shukri Shukriev <shukri@shukriev.eu>
2015-01-09 20:07:45 +0200

    Added paggination of the ads

--- app/js/controllers/HomeController.js
+++ app/js/controllers/HomeController.js
@@ -1,15 +1,28 @@
 'use strict';
 
-// The HomeController holds the presentation logic for the home screen
 app.controller('HomeController',
     function ($scope, $rootScope, adsService, notifyService, pageSize) {
-    	adsService.getAds(null,
-    		function success(data) {
-    			$scope.ads = data;
-    		},
-    		function error (err) {
-    			notifyService.showError("Cannot load ads", err);
-    		}
-    	);
-   }
+        $rootScope.pageTitle = "Home";
+        $rootScope.showRightSidebar = true;
+
+        $scope.adsParams = {
+            'startPage' : 1,
+            'pageSize' : pageSize
+        };
+
+        $scope.reloadAds = function() {
+            adsService.getAds(
+                $scope.adsParams,
+                function success(data) {
+                    $scope.ads = data;
+                    console.log(data);
+                },
+                function error(err) {
+                    notifyService.showError("Cannot load ads", err);
+                }
+            );
+        };
+
+        $scope.reloadAds();
+    }
 );
